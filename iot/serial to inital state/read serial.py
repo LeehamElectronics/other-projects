7ebb4ee47bd63a82fa27fe628a718d3594f826d1
prowlandLATROBE <p.rowland@latrobe.edu.au>
2021-03-20 12:00:57 +1100

    Update read serial.py
    https://stackoverflow.com/questions/16077912/python-serial-how-to-use-the-read-or-readline-function-to-read-more-than-1-char

--- iot/serial to inital state/read serial.py
+++ iot/serial to inital state/read serial.py
@@ -1,5 +1,5 @@
->>> import serial
->>> ser = serial.Serial('/dev/ttyUSB0')  # open serial port
->>> print(ser.name)         # check which port was really used
->>> ser.write(b'hello')     # write a string
->>> ser.close()             # close port
+import serial
+ser = serial.Serial("COM11", 9600)
+while True:
+     cc=str(ser.readline())
+     print(cc[2:][:-5])
