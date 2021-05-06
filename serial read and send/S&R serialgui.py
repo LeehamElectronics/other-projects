65af0c951e31b7dd5b020c491e1d0efdd8eb6d87
acidchu <45096482+acidchu@users.noreply.github.com>
2021-05-06 18:17:14 +1000

    got all the save/log logic done
    f yeh

--- serial read and send/S&R serialgui.py
+++ serial read and send/S&R serialgui.py
@@ -1,27 +1,29 @@
 from tkinter import *
 from tkinter import ttk
-import serial  # pip install pyserial
-import requests  # pip install requests
-import os
-
-logs = []
+import serial
+import requests
 
 
 # =========================================================================
 #
 # =========================================================================
-def run():
+def run_main(main):  # the run_main function just calls the main function every 100ms
+    main()
+    root.after(100, run_main, main)
+
+
+def main():
     if z.get() == "1":
         url = "https://groker.init.st/api/events?accessKey=" + str(access_key.get()) + "&bucketKey=" + str(
             bucket_key.get())  # full url getting general url add variable access_key variable and bucket_key variable
 
         ser = serial.Serial(
-            port=str(port.get()), baudrate=9600, parity=serial.PARITY_NONE,
-            stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
+            port=str(port.get()), baudrate=9600,
+            parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS, timeout=1)
 
         val = 0  # sets val to 0 on start
         print("connected to: " + ser.portstr)
-        #  print(x.get())
 
         while True:
             val = ser.readline()  # reads serial line
@@ -38,28 +40,28 @@ def run():
                 post_name = (str(val)[2:int(str(name_cutoff_e))])
                 variable = (str(val)[int(str(data_cutoff_s)):int(str(data_cutoff_e))])
 
-                post_url = "{}&{}={}".format(url, post_name,
-                                             variable)  # formatting the url - compiles the url, post name and variable into one line , & is for string = for int
+                post_url = "{}&{}={}".format(url, post_name, variable)  # formatting the url
 
                 print(post_url)  # print to console
 
                 if x.get() == "1":
-                    requests.post(
-                        post_url)  # request is the name of a repository and post is the command to post it to the website
+                    requests.post(post_url)
                 else:
                     pass
                 ser.close()
 
 
+def run_read(read):  # the run_read function just calls the read function every 100ms
+    read()
+    root.after(100, run_read, read)
+
 
 def read():
-    number_of_lines = len(open('log.txt').readlines())
-    print(number_of_lines)
+    f = open("log10.txt", "r")  # opens log10.txt in read mode
+    log_list.set(f.read())  # sets log_list to the contents of log10.txt
 
 
-
-def startstoptab1():
-    print(x.get())
+def startstoptab1():  # toggle the main code running. loop always runs just not active
     if z.get() == "1":
         z.set(0)
         start_tab1.set("no")
@@ -68,7 +70,7 @@ def startstoptab1():
         start_tab1.set("yes")
 
 
-def onlinetab1():
+def onlinetab1():  # toggle online or offline
     if x.get() == "1":
         x.set(0)
         online_tab1.set("no")
@@ -78,38 +80,28 @@ def onlinetab1():
 
 
 def save_tab1():
-    print("test")
+    f = open("log.txt", "r")  # opens log.txt in read
+    g = open("exported.txt", "w")  # opens exported.txt in write
+    g.write(f.read())  # copy's the contents of log.txt to exported.txt
+    print("saved")
+    f.close()
+    g.close()
+
+
+def clear_tab1():
+    f = open("log.txt", "w")
+    f.write("")
+    f.close()
 
 
 def save_tab2():
-    access_key.set(tab2_access_key.get())
+    access_key.set(tab2_access_key.get())  # sets the user input to the running
     bucket_key.set(tab2_bucket_key.get())
+    port.set(tab2_port.get())
     name_length.set(tab2_name_length.get())
     data_length.set(tab2_data_length.get())
 
-    url = "https://groker.init.st/api/events?accessKey=" + str(access_key.get()) + "&bucketKey=" + str(
-        bucket_key.get())  # full url getting general url add variable access_key variable and bucket_key variable
 
-    val = "123456789012"
-
-    name_cutoff_s = str("ab")
-    name_cutoff_e = (len(str(name_cutoff_s)) + len(str(name_length.get()))) - len(str(val))
-
-    data_cutoff_s = len(str(name_length.get())) + len(str(name_cutoff_s))
-    data_cutoff_e = (len(str(name_cutoff_s)) + len(str(name_length.get())) + len(str(data_length.get()))) - len(
-        str(val))
-
-    post_name = (str(val)[2:int(str(name_cutoff_e))])
-    variable = (str(val)[int(str(data_cutoff_s)):int(str(data_cutoff_e))])
-
-    post_url = "{}&{}={}".format(url, post_name,
-                                 variable)  # formatting the url - compiles the url, post name and variable into one line , & is for string = for int
-
-    print(post_url)  # print to console
-
-
-# =========================================================================
-#
 # =========================================================================
 
 root = Tk()
@@ -118,8 +110,7 @@ tabControl = ttk.Notebook(root)
 tabControl.pack(expand=1, fill="both")
 
 # =========================================================================
-#
-# =========================================================================
+
 online_tab1 = StringVar()
 start_tab1 = StringVar()
 x = StringVar()
@@ -129,17 +120,29 @@ access_key = StringVar()
 port = StringVar()
 name_length = StringVar()
 data_length = StringVar()
+log_list = StringVar()
+
 # =========================================================================
+
+run_main(main)
+run_read(read)
+
+# =========================================================================
+
 tab1 = ttk.Frame(tabControl)
 tabControl.add(tab1, text='data')
 
-ttk.Button(tab1, text="online/offline", command=onlinetab1).grid(column=1, row=1, sticky=E)
-ttk.Label(tab1, textvariable=online_tab1).grid(column=2, row=1, columnspan=1, sticky=W)
+ttk.Button(tab1, text="on/offline", command=onlinetab1).grid(column=1, row=1, padx=2, pady=2, sticky=E)
+ttk.Label(tab1, textvariable=online_tab1).grid(column=2, row=1, padx=2, pady=2, columnspan=1, sticky=W)
 
-ttk.Button(tab1, text="start/stop", command=startstoptab1).grid(column=1, row=2, sticky=E)
-ttk.Label(tab1, textvariable=start_tab1).grid(column=2, row=2, columnspan=1, sticky=W)
+ttk.Button(tab1, text="start/stop", command=startstoptab1).grid(column=1, row=2, padx=2, pady=2, sticky=E)
+ttk.Label(tab1, textvariable=start_tab1).grid(column=2, row=2, padx=2, pady=2, columnspan=1, sticky=W)
 
-ttk.Button(tab1, text="save", command=save_tab1).grid(column=1, row=12, sticky=E)
+ttk.Button(tab1, text="save", command=save_tab1).grid(column=1, row=3, padx=2, pady=2, sticky=E)
+
+ttk.Button(tab1, text="clear", command=clear_tab1).grid(column=1, row=4, padx=2, pady=2, sticky=E)
+
+ttk.Label(tab1, textvariable=log_list).grid(column=1, row=5, padx=2, pady=2, columnspan=3, sticky=W)
 
 # =========================================================================
 
@@ -147,39 +150,37 @@ tab2 = ttk.Frame(tabControl)
 tabControl.add(tab2, text='setup')
 
 tab2_access_key = StringVar()
-txttab2_access_key = ttk.Entry(tab2, textvariable=tab2_access_key)
-txttab2_access_key.grid(column=2, row=1, columnspan=1, sticky=(W, E))
-ttk.Label(tab2, text="access key").grid(column=1, row=1, sticky=E)
+txt_tab2_access_key = ttk.Entry(tab2, textvariable=tab2_access_key)
+txt_tab2_access_key.grid(column=2, row=1, padx=2, pady=2, columnspan=1, sticky=(W, E))
+ttk.Label(tab2, text="access key").grid(column=1, padx=2, pady=2, row=1, sticky=E)
 
 tab2_bucket_key = StringVar()
-txttab2_bucket_key = ttk.Entry(tab2, textvariable=tab2_bucket_key)
-txttab2_bucket_key.grid(column=2, row=2, columnspan=1, sticky=(W, E))
-ttk.Label(tab2, text="bucket key").grid(column=1, row=2, sticky=E)
+txt_tab2_bucket_key = ttk.Entry(tab2, textvariable=tab2_bucket_key)
+txt_tab2_bucket_key.grid(column=2, row=2, padx=2, pady=2, columnspan=1, sticky=(W, E))
+ttk.Label(tab2, text="bucket key").grid(column=1, row=2, padx=2, pady=2, sticky=E)
 
 tab2_port = StringVar()
-txttab2_port = ttk.Entry(tab2, textvariable=tab2_port)
-txttab2_port.grid(column=2, row=3, columnspan=1, sticky=(W, E))
-ttk.Label(tab2, text="port").grid(column=1, row=3, sticky=E)
-
-tab2_send_test = StringVar()
-txttab2_send_test = ttk.Entry(tab2, textvariable=tab2_send_test)
-txttab2_send_test.grid(column=2, row=4, columnspan=1, sticky=(W, E))
-ttk.Label(tab2, text="send test").grid(column=1, row=4, sticky=E)
+txt_tab2_port = ttk.Entry(tab2, textvariable=tab2_port)
+txt_tab2_port.grid(column=2, row=3, padx=2, pady=2, columnspan=1, sticky=(W, E))
+ttk.Label(tab2, text="port").grid(column=1, row=3, padx=2, pady=2, sticky=E)
 
 tab2_name_length = StringVar()
-txttab2_name_length = ttk.Entry(tab2, textvariable=tab2_name_length)
-txttab2_name_length.grid(column=2, row=5, columnspan=1, sticky=(W, E))
-ttk.Label(tab2, text="name length").grid(column=1, row=5, sticky=E)
+txt_tab2_name_length = ttk.Entry(tab2, textvariable=tab2_name_length)
+txt_tab2_name_length.grid(column=2, row=4, padx=2, pady=2, columnspan=1, sticky=(W, E))
+ttk.Label(tab2, text="name length").grid(column=1, row=4, padx=2, pady=2, sticky=E)
 
 tab2_data_length = StringVar()
-txttab2_data_length = ttk.Entry(tab2, textvariable=tab2_data_length)
-txttab2_data_length.grid(column=2, row=6, columnspan=1, sticky=(W, E))
-ttk.Label(tab2, text="data length").grid(column=1, row=6, sticky=E)
+txt_tab2_data_length = ttk.Entry(tab2, textvariable=tab2_data_length)
+txt_tab2_data_length.grid(column=2, row=5, padx=2, pady=2, columnspan=1, sticky=(W, E))
+ttk.Label(tab2, text="data length").grid(column=1, row=5, sticky=E)
 
-ttk.Button(tab2, text="save", command=save_tab2).grid(column=2, row=8, sticky=E)
+ttk.Button(tab2, text="save", command=save_tab2).grid(column=2, row=6, padx=2, pady=2, sticky=E)
 
-run()
-
-read()
+ttk.Label(tab2, textvariable=start_tab1).grid(column=2, row=7, padx=2, pady=2, columnspan=1, sticky=W)
+ttk.Label(tab2, textvariable=start_tab1).grid(column=2, row=8, padx=2, pady=2, columnspan=1, sticky=W)
+ttk.Label(tab2, textvariable=start_tab1).grid(column=2, row=9, padx=2, pady=2, columnspan=1, sticky=W)
+ttk.Label(tab2, textvariable=start_tab1).grid(column=2, row=10, padx=2, pady=2, columnspan=1, sticky=W)
+ttk.Label(tab2, textvariable=start_tab1).grid(column=2, row=11, padx=2, pady=2, columnspan=1, sticky=W)
+ttk.Label(tab2, textvariable=start_tab1).grid(column=2, row=12, columnspan=1, sticky=W)
 
 root.mainloop()
--- serial read and send/read last 10 lines.py
+++ serial read and send/read last 10 lines.py
@@ -1,32 +1,36 @@
-import os
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 
-def get_last_n_lines(file_name, N):
-    list_of_lines = []
-    with open(file_name, 'rb') as read_obj:
-        read_obj.seek(0, os.SEEK_END)
-        buffer = bytearray()
-        pointer_location = read_obj.tell()
-        while pointer_location >= 0:
-            read_obj.seek(pointer_location)
-            pointer_location = pointer_location -1
-            new_byte = read_obj.read(1)
-            if new_byte == b'\n':
-                list_of_lines.append(buffer.decode()[::-1])
-                if len(list_of_lines) == N:
-                    return list(reversed(list_of_lines))
-                buffer = bytearray()
-            else:
-                buffer.extend(new_byte)
-        if len(buffer) > 0:
-            list_of_lines.append(buffer.decode()[::-1])
-    return list(reversed(list_of_lines))
+def last_ten(file, number_of_lines):
+    h = open("log10.txt", "w")
+    h.write("")
+    f = open(file, "r")
+    lines = []
+    num_lines = sum(1 for line in open('log.txt'))
+    for position, line in enumerate(f):
+        for i in range(num_lines - number_of_lines, num_lines):
+            if position == i:
+                print(line, end='')
+                g = open("log10.txt", "a")  # write file where the lines are sent to
+                g.write(line)
+                lines.append(line)
+    return lines
 
-def main():
-    last_lines = get_last_n_lines("log.txt", 5)
-    for line in last_lines:
-        print(line)
-if __name__ == '__main__':
-   main()
+
+class MyHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        if event.src_path == "./log.txt":
+            last_ten('log.txt', 10)  # file and amount of lines to read
+
+
+event_handler = MyHandler()
+observer = Observer()
+observer.schedule(event_handler, path='./', recursive=False)
+observer.start()
+
+while True:
+    try:
+        pass
+    except KeyboardInterrupt:
+        observer.stop()
