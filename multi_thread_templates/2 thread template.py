2612f14eee4d0730ecfeb2904f2af35e18e6ffd5
acidchu <45096482+acidchu@users.noreply.github.com>
2021-06-02 19:01:37 +1000

    working on multi template

--- multi_thread_templates/2 thread template.py
+++ multi_thread_templates/2 thread template.py
@@ -5,6 +5,11 @@ from time import sleep, perf_counter
 
 # ===================================================================
 # bevan matsacos
+# date 01/06/21
+#
+#
+# threading control start stops kinda scuffed so you gotta start them with an if that's controlled by a button or
+# forever loop to lazy to find a better solution atm
 # ===================================================================
 
 # threading delays
@@ -18,41 +23,41 @@ print(delay_space)
 
 # threading control start stops kinda scuffed so you gotta start them with an if that's controlled by a button or
 # forever loop to lazy to find a better solution atm
-# thread_start = 1
 
-# timing code
-# start_time = perf_counter()
-# end_time = perf_counter()
-# print({end_time - start_time})
-
-
-def test():
-    v = 0
-    while v == 0:
-        print(1)
-        sleep(2)
+# timing code  # use this if you want to see how long it takes your code to do something
+# start_time = perf_counter()  # starts timer
+# end_time = perf_counter()  # ends timer
+# print({end_time - start_time})  # figures out how long it tool
 
 
 # Edward edited your function so it expects an event ("<Button-1>")
 def start_threading(event):  # toggle threading on or off
-    if str(thread_start.get()) == "1":
+    if str(thread_start.get()) == "1":  # if thread_start = 1 set it to 0
         thread_start.set(0)
-        on_off.set("no")
-    else:
+        on_off.set("no")  # set label to no
+    else:  # if its  0 then set it to 1
         thread_start.set(1)
-        on_off.set("yes")
-    threading()
+        on_off.set("yes")  # set label to yes
+    threading()  # runs threading
 
 
-def thread_a():
-    print("a waiting")
-    sleep(delay_start)
-    a = 0
-    while thread_start.get() == str(1):
-        sleep(delay_run)
-        log = "loop a" + " " + str(a)
-        log_list_a.set(log)
-        a = a + 1
+def threading():
+    t1 = Thread(target=thread_a)
+    t2 = Thread(target=thread_b)
+    t1.start()
+    sleep(delay_space)
+    t2.start()
+
+
+def thread_a():  # first thread function
+    print("a waiting")  # tells the threading is running
+    sleep(delay_start)  # delays the start so that it no die
+    a = 0  # creates variable to count
+    while thread_start.get() == str(1):  # infinite loop
+        sleep(delay_run)  # timing delay
+        log = "loop a" + " " + str(a)  # adds strings to the a num
+        log_list_a.set(log)  # set log_list_a to log
+        a = a + 1  # adds one to a each loop
 
 
 def thread_b():
@@ -64,14 +69,6 @@ def thread_b():
         log = "loop b" + " " + str(b)
         log_list_b.set(log)
         b = b + 1
-
-
-def threading():
-    t1 = Thread(target=thread_a)
-    t2 = Thread(target=thread_b)
-    t1.start()
-    sleep(delay_space)
-    t2.start()
 
 
 # =========================================================================
--- multi_thread_templates/Single-threaded.py
+++ multi_thread_templates/Single-threaded.py
@@ -2,22 +2,15 @@ from time import sleep, perf_counter
 
 
 def task():
-    print('Starting a task...')
-    sleep(1)
-    print('done')
+    a = 0
+    v = 0
+    while v == 0:
+        start_time = perf_counter()  # get the value of the performance counter by calling the perf_counter() function:
+        log = "loop a" + " " + str(a)
+        print(log)
+        a = a + 1
+        sleep(1)
+        end_time = perf_counter()  # get the value of the performance counter calling the perf_counter() function
+        print(f'It took {end_time - start_time: 0.2f} second(s) to complete.')  # output the time that takes to complete
 
-
-start_time = perf_counter()  # get the value of the performance counter by calling the perf_counter() function:
-
-task()  # call the task() function twice
-task()
-
-end_time = perf_counter()  # get the value of the performance counter calling the perf_counter() function
-
-print(f'It took {end_time - start_time: 0.2f} second(s) to complete.')  # output the time that takes to complete
-# running the task() function twice:
-
-# First, the task() function executed and slept for one second. Then it executed a second time and also slept for
-# another second. Finally, the program completes. When the task() function called the sleep() function,
-# the CPU didn’t do anything. This is not efficient. This program has one process with a single thread,
-# which is called the main thread. Because the program has only one thread, it’s called the single-threaded program.
+task()  # call the task()
